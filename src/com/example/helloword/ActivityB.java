646ed6f3238af627c307b96b289bf11cdcefacb1
ArryLuo <1394465750@qq.com>
2016-03-05 13:38:41 +0800

    这个是我的第一个Android项目

---
+++ src/com/example/helloword/ActivityB.java
@@ -0,0 +1,13 @@
+package com.example.helloword;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityB extends Activity {
+@Override
+ protected void onCreate(Bundle savedInstanceState) {
+	// TODO Auto-generated method stub
+	super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_b);
+	}
+}
